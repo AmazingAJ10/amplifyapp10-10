83dc88d5d122166eed87823fe5691ac9447f815d
AmazingAJ10 <allenjohnhobbs5792@gmail.com>
2023-11-15 11:14:49 -0500

    go

---
+++ src/ui-components/UINOTECARD.jsx
@@ -0,0 +1,43 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import * as React from "react";
+import { getOverrideProps } from "./utils";
+import ActionCard from "./ActionCard";
+import { Flex } from "@aws-amplify/ui-react";
+export default function UINOTECARD(props) {
+  const { overrides, ...rest } = props;
+  return (
+    <Flex
+      gap="0"
+      direction="column"
+      width="unset"
+      height="unset"
+      justifyContent="flex-start"
+      alignItems="flex-start"
+      position="relative"
+      padding="0px 0px 0px 0px"
+      {...getOverrideProps(overrides, "UINOTECARD")}
+      {...rest}
+    >
+      <ActionCard
+        display="flex"
+        gap="0"
+        direction="column"
+        width="320px"
+        height="unset"
+        justifyContent="center"
+        alignItems="flex-start"
+        shrink="0"
+        position="relative"
+        padding="0px 0px 0px 0px"
+        backgroundColor="rgba(255,255,255,1)"
+        {...getOverrideProps(overrides, "ActionCard")}
+      ></ActionCard>
+    </Flex>
+  );
+}
